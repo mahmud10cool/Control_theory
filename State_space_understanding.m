08eb4516a2d49cc487b217957b25f7621fa9e748
mahmud10cool <mutashi@gmail.com>
2022-09-04 15:56:41 +0600

    Mobile robot kinematic simulation

--- State_space_understanding.m
+++ State_space_understanding.m
@@ -1,9 +1,30 @@
-% Simple understanding of state space
+%% Simple understanding of state space
+% System variables
 M = 1000; k = 2000; B =1000;
 A = [0 1; -k/M -B/M];
 B = [0 ;1/M];
 C = [1 0];
 D = 0;
+% Creating the state space system
 sys = ss(A,B,C,D);
 % Unit step response of the system
 step(sys)
+
+%% Changing state space to transfer function
+[b, a] = ss2tf(A,B,C,D);
+Gs = tf(b,a);
+display(Gs)
+
+%% Testing a controller
+num = poly(-2);
+denom = poly([1 -1 -3]);
+Ps = tf(num,denom);
+display(Ps)
+c_num = poly(1);
+c_denom = poly(0);
+Cs = tf(c_num,c_denom);
+Ts = Ps*Cs;
+display(Ts)
+rlocus(Ts);
+
+
